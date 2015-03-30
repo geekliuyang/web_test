7cda5bcbd9c6c8646fd74cc46b3f64f5d88e5391
liuyang <497384230@qq.com>
2015-03-30 21:07:12 +0800

    增加对输入字数的检测。主要依靠clean_content函数实现。函数以clean+输入栏的name

---
+++ contact/form.py
@@ -0,0 +1,17 @@
+#__author__ = 'yangliu'
+from django import forms
+
+
+class ContactForms(forms.Form):
+    subject = forms.CharField()
+    email = forms.EmailField()
+    content = forms.CharField(widget=forms.Textarea)
+
+    def clean_content(self):
+        content = self.cleaned_data['content']
+        length = len(content)
+
+        if length < 4:
+            raise forms.ValidationError('the words at least 4')
+
+        return content
